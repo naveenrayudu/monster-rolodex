e4ce7075cff91cb969810bca3d1c19c59578daaf
Naveen Rayudu (110 Consulting) <v-narayu@microsoft.com>
2019-07-16 19:24:15 -0700

    Updates

---
+++ precache-manifest.231854a1cad3b5c79c6e702251ab7445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c456ab5cd9c3290b32f810c5803e6de1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "36825cc79e8b6a408258",
+    "url": "/monster-rolodex/static/css/main.0fc9cc3b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "36825cc79e8b6a408258",
+    "url": "/monster-rolodex/static/js/main.4abbefed.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
